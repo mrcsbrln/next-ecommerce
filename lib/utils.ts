b0bb474faabd8807493e0eab29eec47f72f99889
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-10-17 21:31:21 +0200

    setting up shadcn ui

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,4 +1,11 @@
-export function formatPrice(price: number) {
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export function cn(...inputs: ClassValue[]) {
+  return twMerge(clsx(inputs));
+}
+
+export function formatPrice(price: number): string {
   return new Intl.NumberFormat("de-DE", {
     style: "currency",
     currency: "EUR",
