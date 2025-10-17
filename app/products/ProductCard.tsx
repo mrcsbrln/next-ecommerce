031812a0d7ee2b90195287271540f86f477c8d76
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-10-17 21:01:38 +0200

    fetching products from db

--- app/products/ProductCard.tsx
+++ app/products/ProductCard.tsx
@@ -1,18 +1,20 @@
-import { Product } from "@/lib/mocks";
 import { formatPrice } from "@/lib/utils";
 import Image from "next/image";
+import { Product } from "../generated/prisma";
 
 export default function ProductCard({ product }: { product: Product }) {
   return (
     <div className="border border-gray-200 rounded-lg p-4">
       <div className="relative aspect-video">
-        <Image
-          src={product.image}
-          alt={product.name}
-          className="object-cover"
-          fill
-          sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-        />
+        {product.image && (
+          <Image
+            src={product.image}
+            alt={product.name}
+            className="object-cover"
+            fill
+            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+          />
+        )}
       </div>
 
       <h2 className="text-lg font-semibold">{product.name}</h2>
