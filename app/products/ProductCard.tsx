9394ccf85e426cf48524a8be9426d8c4df899c68
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-10-17 21:40:59 +0200

    use shadcn ui for product card

--- app/products/ProductCard.tsx
+++ app/products/ProductCard.tsx
@@ -1,10 +1,17 @@
 import { formatPrice } from "@/lib/utils";
 import Image from "next/image";
 import { Product } from "../generated/prisma";
+import {
+  Card,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 export default function ProductCard({ product }: { product: Product }) {
   return (
-    <div className="border border-gray-200 rounded-lg p-4">
+    <Card className="pt-0 overflow-hidden">
       <div className="relative aspect-video">
         {product.image && (
           <Image
@@ -17,9 +24,13 @@ export default function ProductCard({ product }: { product: Product }) {
         )}
       </div>
 
-      <h2 className="text-lg font-semibold">{product.name}</h2>
-      <p className="text-gray-600">{formatPrice(product.price)}</p>
-      <p className="text-gray-500">{product.description}</p>
-    </div>
+      <CardHeader>
+        <CardTitle>{product.name}</CardTitle>
+        <CardDescription>{product.description}</CardDescription>
+      </CardHeader>
+      <CardFooter>
+        <p>{formatPrice(product.price)}</p>
+      </CardFooter>
+    </Card>
   );
 }
