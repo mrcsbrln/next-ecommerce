b411adc7964298213df04c260fef5bb97b6250d3
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-10-20 20:03:39 +0200

    using suspence with search-params

--- app/page.tsx
+++ app/page.tsx
@@ -9,35 +9,49 @@ import {
 } from "@/components/ui/pagination";
 import ProductCard from "./ProductCard";
 import { prisma } from "@/lib/prisma";
+import { Suspense } from "react";
+import ProductsSkeleton from "./ProductsSkeleton";
 
 type SearchParams = Promise<{ [key: string]: string | string[] | undefined }>;
 
-export default async function HomePage(props: { searchParams: SearchParams }) {
-  const searchParams = await props.searchParams;
+const pageSize = 3;
 
-  const page = Number(searchParams.page) || 1;
-  const pageSize = 3;
+async function Products({ page }: { page: number }) {
   const skip = (page - 1) * pageSize;
 
-  const [products, total] = await Promise.all([
-    prisma.product.findMany({
-      skip,
-      take: pageSize,
-    }),
-    prisma.product.count(),
-  ]);
+  const products = await prisma.product.findMany({
+    skip,
+    take: pageSize,
+  });
 
-  const totalPages = Math.ceil(total / pageSize);
+  // await new Promise((resolve) => setTimeout(resolve, 3000));
 
   return (
-    <main className="container mx-auto p-4">
-      <h1 className="text-3xl font-bold mb-6">Home</h1>
+    <>
       <p>Showing {products.length} products</p>
       <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+    </>
+  );
+}
+
+export default async function HomePage(props: { searchParams: SearchParams }) {
+  const searchParams = await props.searchParams;
+  const page = Number(searchParams.page) || 1;
+
+  const total = await prisma.product.count();
+  const totalPages = Math.ceil(total / pageSize);
+
+  return (
+    <main className="container mx-auto p-4">
+      <h1 className="text-3xl font-bold mb-6">Home</h1>
+
+      <Suspense key={page} fallback={<ProductsSkeleton />}>
+        <Products page={page} />
+      </Suspense>
 
       <Pagination className="mt-8">
         <PaginationContent>
