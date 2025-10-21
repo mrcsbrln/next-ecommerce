8fb7fc4c45449c1ea2764da65174ebefef673458
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-10-21 18:55:18 +0200

    dynamic routes for product page

--- app/ProductCard.tsx
+++ app/ProductCard.tsx
@@ -8,29 +8,32 @@ import {
   CardHeader,
   CardTitle,
 } from "@/components/ui/card";
+import Link from "next/link";
 
 export default function ProductCard({ product }: { product: Product }) {
   return (
-    <Card className="pt-0 overflow-hidden">
-      <div className="relative aspect-video">
-        {product.image && (
-          <Image
-            src={product.image}
-            alt={product.name}
-            className="object-cover"
-            fill
-            sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
-          />
-        )}
-      </div>
+    <Link href={`/product/${product.slug}`}>
+      <Card className="pt-0 overflow-hidden">
+        <div className="relative aspect-video">
+          {product.image && (
+            <Image
+              src={product.image}
+              alt={product.name}
+              className="object-cover"
+              fill
+              sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+            />
+          )}
+        </div>
 
-      <CardHeader>
-        <CardTitle>{product.name}</CardTitle>
-        <CardDescription>{product.description}</CardDescription>
-      </CardHeader>
-      <CardFooter>
-        <p>{formatPrice(product.price)}</p>
-      </CardFooter>
-    </Card>
+        <CardHeader>
+          <CardTitle>{product.name}</CardTitle>
+          <CardDescription>{product.description}</CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <p>{formatPrice(product.price)}</p>
+        </CardFooter>
+      </Card>
+    </Link>
   );
 }
