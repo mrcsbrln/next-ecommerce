19e781f25091da1cedfcecce7b7b56b700613358
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-10-22 12:11:55 +0200

    product page with images

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -3,6 +3,7 @@ import { Card, CardContent } from "@/components/ui/card";
 import { Separator } from "@/components/ui/separator";
 import { getProductsBySlug } from "@/lib/actions";
 import { formatPrice, sleep } from "@/lib/utils";
+import Image from "next/image";
 import { notFound } from "next/navigation";
 
 export async function generateMetadata({
@@ -49,18 +50,33 @@ export default async function ProductPage({
   return (
     <main className="container mx-auto p-4">
       <Card className="max-w-3xl mx-auto">
-        <CardContent className="p-6">
-          <h1 className="text-3xl font-bold mb-2">{product.name}</h1>
-          <div className="flex items-center gap-2 mb-4">
-            <span className="font-semibold text-lg">
-              {formatPrice(product.price)}
-            </span>
-            <Badge variant="outline">{product.category?.name}</Badge>
+        <CardContent className="p-6 grid grid-cols-1 md:grid-cols-2 gap-4">
+          {" "}
+          <div className="relative rounded-lg overflow-hidden">
+            {product.image && (
+              <Image
+                src={product.image}
+                alt={product.name}
+                fill
+                priority
+                sizes="(max-width: 768px) 100vw, 50vw"
+                className="object-cover"
+              />
+            )}
           </div>
-          <Separator className="my-4" />
-          <div className="space-y-2">
-            <h2 className="font-medium">Description</h2>
-            <p>{product.description}</p>
+          <div>
+            <h1 className="text-3xl font-bold mb-2">{product.name}</h1>
+            <div className="flex items-center gap-2 mb-4">
+              <span className="font-semibold text-lg">
+                {formatPrice(product.price)}
+              </span>
+              <Badge variant="outline">{product.category?.name}</Badge>
+            </div>
+            <Separator className="my-4" />
+            <div className="space-y-2">
+              <h2 className="font-medium">Description</h2>
+              <p>{product.description}</p>
+            </div>
           </div>
         </CardContent>
       </Card>
