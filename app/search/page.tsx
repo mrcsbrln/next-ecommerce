ab6a9db0286d04f9bcdc4e2ab2417c4143eb3a8e
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-11-18 21:36:12 +0100

    search feature progress

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -1,13 +1,62 @@
+import Breadcrumbs from "@/components/breadcrumbs";
+import ProductCard from "../ProductCard";
+import { prisma } from "@/lib/prisma";
+import { sleep } from "@/lib/utils";
+import { Suspense } from "react";
+import ProductsSkeleton from "../ProductsSkeleton";
+
 type SearchPageProps = {
   searchParams: Promise<{ query?: string }>;
 };
 
+async function Products({ query }: { query: string }) {
+  const products = await prisma.product.findMany({
+    where: {
+      OR: [
+        { name: { contains: query, mode: "insensitive" } },
+        { description: { contains: query, mode: "insensitive" } },
+      ],
+    },
+    take: 18,
+  });
+
+  await sleep(1000); //Simulate a delay for loading
+
+  if (products.length === 0) {
+    return (
+      <div className="text-center text-muted-foreground">No products found</div>
+    );
+  }
+
+  return (
+    <>
+      <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-3">
+        {products.map((product) => (
+          <ProductCard key={product.id} product={product} />
+        ))}
+      </div>
+    </>
+  );
+}
+
 export default async function SearchPage({ searchParams }: SearchPageProps) {
   const params = await searchParams;
+  const query = params.query?.trim() ?? "";
+
+  const breadcrumbs = [
+    { label: "Products", href: "/" },
+    {
+      label: `Results for "${query}"`,
+      href: `/search?query=${encodeURIComponent(query)}`,
+    },
+  ];
+
   return (
-    <div className="container mx-auto py-4">
-      <h1>Search</h1>
-      <p>The query is {params.query ?? "not provided"}</p>
-    </div>
+    <main className="container mx-auto py-4">
+      <Breadcrumbs items={breadcrumbs} />
+      <Suspense key={query} fallback={<ProductsSkeleton />}>
+        <Products query={query} />
+      </Suspense>
+    </main>
   );
 }
