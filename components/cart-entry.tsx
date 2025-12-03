7709c5fa4ab0fe8aa9ec83a3a54a6e492257ddde
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-12-03 16:07:28 +0100

    increasing and decreasing cart item quantity

--- components/cart-entry.tsx
+++ components/cart-entry.tsx
@@ -1,14 +1,37 @@
-import { cartItemWithProduct } from "@/lib/actions";
+"use client";
+import { cartItemWithProduct, setProductQuantity } from "@/lib/actions";
 import { formatPrice } from "@/lib/utils";
 import Image from "next/image";
 import { Button } from "./ui/button";
 import { Minus, Plus } from "lucide-react";
+import { useState } from "react";
 
 interface CartEntryProps {
   cartItem: cartItemWithProduct;
 }
 
 export default function CartEntry({ cartItem }: CartEntryProps) {
+  const [isLoading, setIsLoading] = useState(false);
+  const handleIncrement = async () => {
+    setIsLoading(true);
+    try {
+      await setProductQuantity(cartItem.product.id, cartItem.quantity + 1);
+    } catch (error) {
+      console.error("Error incrementing cart item:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  const handleDecrement = async () => {
+    setIsLoading(true);
+    try {
+      await setProductQuantity(cartItem.product.id, cartItem.quantity - 1);
+    } catch (error) {
+      console.error("Error decrementing cart item:", error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
   return (
     <li className="border-b border-muted flex py-4 justify-between">
       <div className="flex space-x-4">
@@ -28,11 +51,21 @@ export default function CartEntry({ cartItem }: CartEntryProps) {
       <div className="flex flex-col justify-between items-end gap-2">
         <p className="font-medium">{formatPrice(cartItem.product.price)}</p>
         <div className="flex items-center border border-muted rounded-full">
-          <Button variant="ghost" className="rounded-l-full">
+          <Button
+            variant="ghost"
+            className="rounded-l-full"
+            onClick={handleDecrement}
+            disabled={isLoading}
+          >
             <Minus className="h-4 w-4" />
           </Button>
           <p className="w-6 text-center">{cartItem.quantity}</p>
-          <Button variant="ghost" className="rounded-r-full">
+          <Button
+            variant="ghost"
+            className="rounded-r-full"
+            onClick={handleIncrement}
+            disabled={isLoading}
+          >
             <Plus className="h-4 w-4" />
           </Button>
         </div>
--- lib/actions.ts
+++ lib/actions.ts
@@ -185,7 +185,6 @@ export async function setProductQuantity(productId: string, quantity: number) {
           productId,
         },
       });
-      revalidateTag(`cart_${cart.id}`);
     } else {
       await prisma.cartItem.updateMany({
         where: {
@@ -197,6 +196,7 @@ export async function setProductQuantity(productId: string, quantity: number) {
         },
       });
     }
+    revalidateTag(`cart-${cart.id}`);
   } catch (error) {
     console.error("Error updating cart item quantity:", error);
     throw new Error("Failed to update cart item quantity");
