8c1008b00f19cfdbb9a75e7868f2466cd12c4e9d
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-12-08 21:03:25 +0100

    stripe checkout session

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,3 +1,4 @@
+import { Prisma } from "@/app/generated/prisma";
 import Stripe from "stripe";
 
 if (!process.env.STRIPE_SECRET_KEY) {
@@ -8,3 +9,15 @@ export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
   apiVersion: "2025-11-17.clover",
   typescript: true,
 });
+
+export type OrderWithItemsAndProduct = Prisma.OrderGetPayload<{
+  include: {
+    items: {
+      include: {
+        product: true;
+      };
+    };
+  };
+}>;
+
+export async function createCheckoutSession(order: OrderWithItemsAndProduct) {}
