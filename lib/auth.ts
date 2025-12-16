6aa4e33e718232a53e0c8c11e7ac830d980bf4fc
mrcsbln <hrtmnn.mrcs@gmail.com>
2025-12-16 21:21:58 +0100

    hashing passwords

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,5 +1,11 @@
+import bcrypt from "bcryptjs";
 import NextAuth from "next-auth";
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [],
 });
+
+export async function hashPassword(password: string) {
+  const saltRounds = 10;
+  return await bcrypt.hash(password, saltRounds);
+}
