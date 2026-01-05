16df38f186f9d4d8ca5c62dad317124aeb1f9d40
mrcsbln <hrtmnn.mrcs@gmail.com>
2026-01-05 15:10:33 +0100

    auth status ui and logout

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -5,6 +5,7 @@ import SearchInput from "./search-input";
 import CartIndicator from "./cart-indicator";
 import { Suspense } from "react";
 import CartIndicatorSkeleton from "./cart-indicator-skeleton";
+import AuthStatus from "./auth-status";
 
 export const categories = [
   {
@@ -51,6 +52,7 @@ export default function Navbar() {
         </div>
 
         <div className="flex items-center gap-0">
+          <AuthStatus />
           <Suspense fallback={<CartIndicatorSkeleton />}>
             <CartIndicator />
           </Suspense>
