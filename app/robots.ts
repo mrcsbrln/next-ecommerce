6ab4f371c2d91570920fee310cbb0ea865eddbff
mrcsbln <hrtmnn.mrcs@gmail.com>
2026-01-23 12:24:17 +0100

    add imprint and privacy page and edit robots.ts

--- app/robots.ts
+++ app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: "*",
       allow: "/",
-      disallow: "/private/",
+      disallow: ["/private/", "/imprint", "/privacy"],
     },
     sitemap: `${process.env.NEXT_PUBLIC_URL}/sitemap.xml`,
   };
